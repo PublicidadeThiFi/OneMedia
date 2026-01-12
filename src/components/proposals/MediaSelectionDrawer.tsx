9ba0cc7b1d56afc98085d685ab8f8b79df32340d
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2026-01-12 19:45:14 -0300

    fix widht 2

--- src/components/proposals/MediaSelectionDrawer.tsx
+++ src/components/proposals/MediaSelectionDrawer.tsx
@@ -300,7 +300,8 @@ export function MediaSelectionDrawer({
       }}
     >
       <DialogContent
-        className="flex flex-col p-0 gap-0 w-[96vw] sm:w-[94vw] md:w-[94vw] lg:w-[92vw] max-w-7xl h-[90vh] max-h-[90vh] overflow-hidden"
+        style={{ width: '96vw', maxWidth: 'min(1400px, 96vw)', height: '90vh', maxHeight: '90vh' }}
+        className="flex flex-col p-0 gap-0 overflow-hidden"
       >
         <DialogHeader className="px-6 py-4 border-b">
           <DialogTitle>Selecionar Mídia do Inventário</DialogTitle>
